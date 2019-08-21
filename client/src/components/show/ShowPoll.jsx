c10f6de3b660f12efb496f46bea1bfe60925f06a
Erinn Nelson <erinn@Erinns-MacBook-Pro.local>
2019-08-21 10:09:22 -0400

    add copy URL button to show poll

--- client/src/components/show/ShowPoll.jsx
+++ client/src/components/show/ShowPoll.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import { fetchPoll, addUserVote } from '../../services/api-helper'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 
 import VoteChoice from './VoteChoice'
@@ -12,7 +13,8 @@ class ShowPoll extends React.Component {
       poll: null,
       choices: [],
       votes: 0,
-      alreadyVoted: false
+      alreadyVoted: false,
+      copied: false
     })
   }
 
@@ -37,10 +39,24 @@ class ShowPoll extends React.Component {
     })
   }
 
-  flipBack = () => {
+  flipAlreadyVotedBack = () => {
     this.setState({
       alreadyVoted: false
     })
+  }
+
+  flipCopiedBack = () => {
+    this.setState({
+      copied: false
+    })
+  }
+
+  copyURL = () => {
+    console.log('hello')
+    this.setState({
+      copied: true
+    })
+    setTimeout(this.flipCopiedBack, 2000)
   }
 
   userVote = async (choiceId) => {
@@ -60,26 +76,36 @@ class ShowPoll extends React.Component {
       this.setState({
         alreadyVoted: true
       })
-      setTimeout(this.flipBack, 2000)
+      setTimeout(this.flipAlreadyVotedBack, 2000)
     }
   }
 
   render() {
     return (
-      <div id="show-poll-container">
-        <h2>{this.state.poll && this.state.poll.title}</h2>
-        {this.state.choices.map(choice => (
-          <div key={choice.id}>
-            <VoteChoice
-              choice={choice}
-              votes={this.state.votes}
-              userVote={this.userVote}
-            />
+      <div id='show-poll-outer-container'>
+        <div id='show-poll-container'>
+          <h2>{this.state.poll && this.state.poll.title}</h2>
+          {this.state.choices.map(choice => (
+            <div key={choice.id}>
+              <VoteChoice
+                choice={choice}
+                votes={this.state.votes}
+                userVote={this.userVote}
+              />
 
-          </div>
-        ))}
-        <p className='total-votes'>{this.state.votes} total votes</p>
-        {this.state.alreadyVoted ? "Already Voted" : ""}
+            </div>
+          ))}
+          <p className='total-votes'>{this.state.votes} total votes</p>
+          {this.state.alreadyVoted ? <p id='already-voted-error'>Already Voted</p> : ''}
+        </div>
+        {this.state.copied
+          ?
+          <p id='url-copy-success'>COPIED!</p>
+          :
+          <CopyToClipboard text={window.location.href}>
+            <p id='url-copy-request' onClick={this.copyURL}>Copy URL</p>
+          </CopyToClipboard>
+        }
       </div>
     );
   }
